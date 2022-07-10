d1ccf9b6511a29142ca182fc91e07dd7c4027492
Michał Biel <micha@kumulugma.pl>
2022-07-10 08:21:29 +0200

    0.0.1a - Podstawowa wersja.

---
+++ K3e-Plugin-Reset.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  Plugin name: K3e - Plugin Reset
+  Plugin URI: https://www.k3e.pl/
+  Description: Przystawka K3e do kasowania konfiguracji K3e-Plugin.
+  Author: K3e
+  Author URI: https://www.k3e.pl/
+  Text Domain:
+  Domain Path:
+  Version: 0.0.1a
+ */
+require_once 'updater/K3eUpdater.php';
+add_action('init', 'k3e_plugin_init');
+
+Puc_v4_Factory::buildUpdateChecker(
+        'http://k3e.pl/?action=get_metadata&slug=k3e-plugin-reset',
+        __FILE__, //Full path to the main plugin file or functions.php.
+        'k3e-plugin-reset'
+);
+
+function k3e_plugin_init() {
+    do_action('k3e_plugin_init');
+}
+
+function k3e_plugin_activate() {
+   update_option('k3e_system_modules', []);
+}
+
+register_activation_hook(__FILE__, 'k3e_plugin_activate');
+
+function k3e_plugin_deactivate() {
+}
+
+register_deactivation_hook(__FILE__, 'k3e_plugin_deactivate');
---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center">
+    <a href="https://k3e.pl" target="_blank">
+        <img src="http://k3e.pl/images/icons/k3e/100x100.png" height="100px">
+    </a>
+    <h1 align="center">Podstawowa do kasowania konfiguracji K3e-Plugin</h1>
+    <br>
+</p>
+
+Wersje
+-------------------
+0.0.1a
+- Podstawowa wersja.
